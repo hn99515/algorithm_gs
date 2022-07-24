98e957fdb3d73f9b5cf58cda6d0d31ba0deb6846
Sarah-Sohyun-Park <sarahpark1kr@gmail.com>
2022-07-24 16:37:10 +0900

    [BOJ]_1-14

--- sohyun/study0726.py
+++ sohyun/study0726.py
@@ -40,3 +40,57 @@ numbers_2 = input().split()
 print(int(numbers[0]) - int(numbers[1]))
 #7 #8
 # 곱하기는 * 나누기는 / 를 활용한다
+
+#9
+numbers_3 = map(int, input().split())
+print(numbers_3[0] + numbers_3[1])
+print(numbers_3[0] - numbers_3[1])
+print(numbers_3[0] * numbers_3[1])
+print(int(numbers_3[0] / numbers_3[1]))
+print(numbers_3[0] % numbers_3[1])
+
+#10
+user_names = ['joonas', 'beakjoon']
+add_names = input()
+if add_names in user_names:
+    print(add_names + '??!')
+
+#리스트 안에 이름을 넣고 그것과 겹치는 것이 있는지 확인하기 위해 in을 쓴다.
+# 이후 if 문으로 조건이 충족 되었을 때 뒤에 '??1'를 추가하여 출력한다.
+
+#11
+y = int(input())
+print(y-543)
+
+#12
+numbers_3 = map(int, input().split())
+print((numbers_3[0] + numbers_3[1]) % numbers_3[2])
+print( ((numbers_3[0] % numbers_3[2]) + (numbers_3[1] % numbers_3[2])) % numbers_3[2])
+print((numbers_3[0] * numbers_3[1]) % numbers_3[2])
+print(((numbers_3[0] % numbers_3[2]) * (numbers_3[1] % numbers_3[2])) % numbers_3[2])
+
+def operation_test(A, B, C):
+    print((A + B) % C)
+    print(((A % C) + (B % C)) % C)
+    print((A * B) % C)
+    print((A % C) * (B % C) % C)
+
+print(operation_test(5, 8, 4))
+
+#13
+number_4 = int(input())
+number_5 = input()
+number_5_original = int(number_5)
+number_5_reverse = list(number_5)[::-1]
+
+for i in number_5:
+    print(number_4 * int(i))
+print(number_4 *number_5_original)
+
+#
+print('         ,r\'\"7')
+print('r`-_   ,\'  ,/')
+print( '\\. \". L_r\'')
+print('   `~\\/')
+print('      |')
+print('      |')
