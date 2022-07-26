d6e97abaa460acbf72650fb3ddd5c9708ce28ac3
recorror <itgwd9701@gmail.com>
2022-07-26 21:33:33 +0900

    BOJ / 21문제 / 걸린시간 : 오래

---
+++ Hajinwoo/BOJ/BOJ_9498.py
@@ -0,0 +1,12 @@
+a = int(input())
+100 >= a >= 0
+if 100 >= a >=90:
+    print('A')
+elif 90 > a >= 80:
+    print('B')
+elif 80 > a >= 70:
+    print('C')
+elif 70 > a >= 60:
+    print('D')
+else :
+    print('F')
