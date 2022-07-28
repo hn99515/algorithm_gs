5c413b4593be83c23fb237f7b4d31a94d8d8df63
heejucherish <gmlwn1954@gmail.com>
2022-07-28 17:59:54 +0900

    boj 14 solved

---
+++ heeju/BOJ/0727/[BOJ]1110.py
@@ -0,0 +1,16 @@
+# 더하기 사이클 
+n = int(input())
+num = n
+cnt = 0
+
+while True:
+    a = num // 10
+    b = num % 10
+    c = (a+b) % 10
+    num = (b*10) + c
+    cnt += 1
+    if (num == n):
+        break
+    
+
+print(cnt)
