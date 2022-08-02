2f2b83ebc124ffc4af1acbe7bcb627e9f63e0fb6
Awake-Ukey <hn99515@gmail.com>
2022-08-02 23:58:17 +0900

    0802 string

--- Donguk/string/[boj]1316.py
+++ Donguk/string/[boj]1316.py
@@ -1,19 +1,13 @@
 N = int(input())
+res = N                                 # 입력값(개수)을 넣고 시작(조건에 맞지 않는 것은 제외시킨다.)
 
-for i in range(N):
+for i in range(N):                      # 개수만큼 순회
     words = input()
-
-    count = 0
-    for l in range(len(words)):
-        if words[l] != words[l+1]:
-            if words[l] not in words[l+2:]:
-                count += 1
-        else:
-            if words[l] != words[l+2:]:
-                if words[l] not in words[l+2:]:
-                    count += 1
-            else:
-                
-
-            
-        
+    
+    for j in range(len(words)-1):       # 단어의 인덱스만큼 순회
+        if words[j] == words[j+1]:      # 그 다음 문자와 같으면 pass
+            pass
+        elif words[j] in words[j+1:]:   # 끝까지 확인하여 같은게 있다면 -1 (그룹단어 제외)
+            res -= 1
+            break                       # 반복문 종료
+print(res)        
