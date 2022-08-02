e5dfa2d99f410d435b6fe8d444d1cbad8244c467
Awake-Ukey <hn99515@gmail.com>
2022-08-02 21:56:09 +0900

    0802 string

--- Donguk/string/[boj]1152.py
+++ Donguk/string/[boj]1152.py
@@ -1,9 +1,3 @@
-# 단어 수 = 공백 개수 +1
-
-sentence = input()
-cnt = 1
-for word in sentence:
-    if word == ' ':
-        cnt += 1
-
-print(cnt)
+# 공백이 기준이다!
+sentence = input().split()  # split() 함수는 리스트로 반환
+print(len(sentence))
