6665e023c793732f0db287b6af4e4155c5f2c401
Awake-Ukey <hn99515@gmail.com>
2022-08-04 21:58:02 +0900

    [BOJ] Basic Math

--- Donguk/math_basic/[boj]2869.py
+++ Donguk/math_basic/[boj]2869.py
@@ -1,7 +1,12 @@
 # 달팽이가 올라가는 높이 A, 내려가는 높이 B, 나무 길이 V
 # 올라갔다가 내려온 높이가 나무 길이보다 작으면 무한 루프
+import math
 
 A, B, V = map(int, input().split())
+
+oneday = (V - B) / (A - B)          # 하루에 최대 올라간 적 있는 높이(V에 닿기만 해도 안내려오기 때문!)
+print(math.ceil(oneday))
+
 # h = 0           # 달팽이의 높이
 # cnt = 0         # 횟수 체크
 
@@ -15,9 +20,9 @@ A, B, V = map(int, input().split())
         
 # print(cnt)
 
-if A - B > 0:
-    cnt = V - A + 1
-elif A == V:
-    cnt = 1
+# if A - B > 0:
+#     cnt = V - A + 1
+# elif A == V:
+#     cnt = 1
 
-print(cnt)
+# print(cnt)
