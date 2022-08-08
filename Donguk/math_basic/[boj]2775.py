551017fe06766d0958aac85aa0c3e8a521dcc2c5
Awake-Ukey <hn99515@gmail.com>
2022-08-09 00:16:22 +0900

    0808 basic_math

--- Donguk/math_basic/[boj]2775.py
+++ Donguk/math_basic/[boj]2775.py
@@ -1,7 +1,12 @@
 T = int(input())
 
-for j in range(2):
-    k = int(input())
-    n = int(input())
+for _ in range(T):
+    f = int(input())                    # 층
+    n = int(input())                    # 호
+    arr = [x for x in range(1, n+1)]    # 0층의 사람 수를 리스트로 저장
 
+    for k in range(f):                  # 각 층을 순회
+        for i in range(1, n):           # 각 호실을 중복 순회
+            arr[i] += arr[i-1]          # 사람 수 업데이트
+    print(arr[-1])
     
