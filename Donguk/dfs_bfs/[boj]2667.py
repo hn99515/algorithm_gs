0c48a27713f1be19d174c285995ebe73af7b1b74
Awake-Ukey <hn99515@gmail.com>
2022-09-14 00:52:03 +0900

    0914 algorithm dfs bfs

--- Donguk/dfs_bfs/[boj]2667.py
+++ Donguk/dfs_bfs/[boj]2667.py
@@ -4,26 +4,37 @@
 지도를 입력하여 단지수를 출력하고, 각 단지에 속하는 집의 수를 오름차순으로 정렬하여 출력
 각 단지에 속하는 집의 수를 구한 후 리스트에 담아주면 len()으로 단지수 출력 가능
 '''
-def bfs(i, j, N):
-    visited = [[0] * N for _ in range(N)]
+def bfs(i, j):
     q = []
     q.append((i, j))
     visited[i][j] = 1
+    cnt = 1
     while q:
         i, j = q.pop(0)
-        if apt[i][j] == 1:
-            return visited[i][j]
 
         for di, dj in [[0, 1], [1, 0], [0, -1], [-1, 0]]:
-            ni, nj = i + di, j + dj
-            if 0 <= ni < N and 0 <= nj < N and apt[ni][nj] != 0 and visited[ni][nj] == 0:
+            ni, nj = i+di, j+dj
+            if 0 <= ni < N and 0 <= nj < N and visited[ni][nj] == 0 and apt[ni][nj] == 1:
                 q.append((ni, nj))
-                visited[ni][nj] == visited[i][j] + 1
+                visited[ni][nj] = visited[i][j] + 1
+                cnt += 1
+    return cnt
 
 
 N = int(input())
 apt = [list(map(int, input())) for _ in range(N)]
 
+ans = []        # 세대수를 모을 리스트
 
-print(bfs(i, j, N))
+visited = [[0] * N for _ in range(N)]
+for i in range(N):
+    for j in range(N):
+        # 아파트 & 방문한적 없는 경우
+        if apt[i][j] == 1 and visited[i][j] == 0:
+            ans.append(bfs(i, j))
+
+ans.sort()
+print(len(ans))
+for i in range(len(ans)):
+    print(ans[i])
 
