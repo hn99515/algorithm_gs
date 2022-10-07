9a6a011c108fb19d2a803bcadc0544689de04cc3
Awake-Ukey <hn99515@gmail.com>
2022-10-07 17:55:45 +0900

    1007 [SWEA]6190

---
+++ Donguk/6190_increase_num/[SWEA]6190.py
@@ -0,0 +1,21 @@
+'''
+단조 증가하는 수인데, 각 숫자의 자릿수가 단순하게 증가하는 수를 말한다.
+어떤 k자리 수 X = d1d2…dk 가 d1 ≤ d2 ≤ … ≤ dk 를 만족하면 단조 증가하는 수
+
+ 1 ≤ i < j ≤ N 인 두 i, j에 대해, Ai x Aj값이 단조 증가하는 수인 것들을 구하고 그 중의 최댓값을 출력
+'''
+import sys
+sys.stdin = open('s_input.txt')
+
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    arr = list(map(int, input().split()))
+
+    res = []
+    for i in range(N-1):
+        for j in range(i+1, N):
+            res.append(arr[i] * arr[j])
+
+    for k in range(len(res)):
+        if res[k] % 10
